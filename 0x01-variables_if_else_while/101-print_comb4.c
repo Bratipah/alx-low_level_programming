5d07f67384cfe51a5e673b280e77907fd7b1bf92
Bratipah <bratipahmh@gmail.com>
2022-06-03 14:29:22 -0700

    Do what you do better

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combinations of
+ * 3 digits
+ * Nos to be separated by ,followed by a space
+ * 2 digits must be different
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * Return 0
+ */
+
+int main(void)
+{
+		int n1 = 48;
+		int n2, n3;	
+		int com = 44;
+
+		while (n1 <= 57)
+		{
+			n2= n2 +1;
+			while (n3 <= 57)
+			{
+				n3= n2+1;
+				while(n3 <= 57)
+				{
+					putchar(n1);
+					putchar(n2);
+					putchar(n3);
+					if (n1 !=55 || n2 !=56 || n3 !=57)
+					{
+						putchar(com);
+						putchar(32);
+					}
+					n3 += 1;
+				}
+				n2 += 1;
+			}
+			n1 += 1;
+		}
+		putchar('\n');
+		return (0);
+}
+
