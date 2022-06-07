2188a3a5ba35a20b5a5e54097d1e312d749d4d87
Bratipah <bratipahmh@gmail.com>
2022-06-07 16:32:59 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * print_last_digit - Prints the last digit
  * of a number.
@@ -17,13 +15,10 @@ int print_last_digit(int n)
 	int l;
 
 	l = n % 10;
-
 	if (l < 0)
 		{
 		_putchar(-l + 48);
-	return (-l);
-										   }
-
+	return (-l);								   }
 	else
 	{
 	_putchar(l + 48);
