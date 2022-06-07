87ff8cb5a18390ad278f3ffb7bbe051638d0e19f
Bratipah <bratipahmh@gmail.com>
2022-06-07 14:14:14 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print the word "mael"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i;
+	char p[] = "mael";
+
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+
+		}
+		_putchar('\n');
+		return (0);
+}
