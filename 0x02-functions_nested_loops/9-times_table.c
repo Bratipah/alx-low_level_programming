d98633b68ee281f8516149fd94994002e36491dd
Bratipah <bratipahmh@gmail.com>
2022-06-08 05:53:00 -0700

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,6 +5,7 @@
  *
  * Return: no return
  */
+
 void times_table(void)
 {
 		int a, b, c;
@@ -12,7 +13,7 @@ void times_table(void)
 		for (a = 0; a <= 9; a++)
 		{
 			_putchar(48);
-			for (b = 1; b <= 9; b++)
+			for (b = 1; b<= 9; b++)
 			{
 				c = a * b;
 				_putchar(44);
@@ -27,7 +28,6 @@ void times_table(void)
 					_putchar((c / 10) + 48);
 					_putchar((c % 10) + 48);
 				}
-			}			
 		_putchar('\n');
 		}
 }
