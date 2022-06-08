3dab5c98e4ec0a1348b23734dbce8131d6a77e7a
Bratipah <bratipahmh@gmail.com>
2022-06-07 17:32:20 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,8 +14,10 @@ void jack_bauer(void)
 	{
 		for (b = 48; b <= 57; b++)
 		{
+
 			for (c = 48; c <= 53; c++)
-			{									  for (d = 48; d <= 57; d++)
+			{									  
+				for (d = 48; d <= 57; d++)
 				{		
 					if (a >= 50 && b >= 52)
 					break;
