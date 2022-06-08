0940f90e5deb9ad61d6a04fd60526cca8d949411
Bratipah <bratipahmh@gmail.com>
2022-06-07 18:59:25 -0700

    TimesTable:

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,22 +12,19 @@ void times_table(void)
 
 		for (a = 0; a <= 9; a++)
 		{
-			_putchar(48);
-			for (b = 1; b <= 9; b++)
+			c = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
 			{
-														c = a * b;
-														_putchar(44);
-														_putchar(32);
-														if (c <= 9)
-														{
-															_putchar(32);
-															_putchar(c + 48);
-														}										else
-														{
-															_putchar((c / 10) + 48);
-															_putchar((c % 10) + 48);
-														}
-													}
-			_putchar('\n');
+				_putchar(32);
+				_putchar(c + 48);
 			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		_putchar('\n');
+		}
 }
