b1d2eda52762a5010692061e926f14f2e5584177
Bratipah <bratipahmh@gmail.com>
2022-06-08 16:15:58 -0700

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,8 +16,10 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month > 2 && day >= 60)
 		{
-			day++;								}
-		printf("Day of the year: %d\n", day);					printf("Remaining days: %d\n", 366 - day);
+			day++;
+		{		
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
