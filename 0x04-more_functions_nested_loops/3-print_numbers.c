0dcc91409e82211c002caceb1a0d26b373fc3937
Bratipah <bratipahmh@gmail.com>
2022-06-09 13:24:31 -0700

    The numbers speak

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,22 +1,15 @@
-nclude "holberton.h"
-
-
+#include "main.h"
 
 /**
- *
- * * print_numbers - function that prints the numbers, from 0 to 9.
- *
- * */
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ */
 
 void print_numbers(void)
 {
 
 	char c;
 
-
-
 	for (c = '0'; c <= '9'; c++)
-
 	_putchar(c);
 	_putchar('\n');
 }
