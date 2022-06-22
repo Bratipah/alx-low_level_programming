dc95e1123f1469a4be60715babc0cfd996df5171
Bratipah <bratipahmh@gmail.com>
2022-06-22 08:23:44 -0700

    recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_puts_recursion("Betty Holberton");
+	return (0);
+}
