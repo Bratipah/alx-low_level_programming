70939ebfb7bc9871768ca2ecc4f60d4165a13ff6
Bratipah <bratipahmh@gmail.com>
2022-06-22 10:13:37 -0700

    wildcamp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,17 +4,16 @@
  * @s2: second string containing '*' or not
  * Return: 1 if the strings can be considered as identical, and 0
  */
- int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
 		if (*s2 != '\0' && *s2 == '*')
-		{	
+		{
 			return (wildcmp(s1, s2 + 1));
 		}
 		return (*s2 == '\0');
 	}
-	
 	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
