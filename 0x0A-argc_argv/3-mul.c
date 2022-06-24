8677b899b764a16adaea03f30b96f91c1a85ab00
Bratipah <bratipahmh@gmail.com>
2022-06-24 15:07:30 -0700

    args3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - multiplies two numbers. 
+ * main - multiplies two numbers.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
