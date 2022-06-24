a0779fe9d39d73d219f726957f6abec086d01c8d
Bratipah <bratipahmh@gmail.com>
2022-06-24 14:39:15 -0700

    whatsmyname

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -6,9 +6,8 @@
  * @argv: array of pointers to arguement strings
  * Return: 0
  **/
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	argc = argc;
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
