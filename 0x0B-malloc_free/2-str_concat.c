3a656cf7ff8f144f2fe0d73845c7f44d3a7c62b3
Bratipah <bratipahmh@gmail.com>
2022-06-28 15:07:35 -0700

    take risks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	
+
 	strout = malloc(sizeof(char) * (i + j + 1));
 
 	if (strout == NULL)
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
-		
+
 	limit = j;
 	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
