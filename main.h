8f739eaa585e729f1c5b047d1b20fbee97c72a44
Bratipah <bratipahmh@gmail.com>
2022-07-04 16:02:09 -0700

    object like macro

--- main.h
+++ main.h
@@ -1,5 +1,10 @@
-#i`fndef _MAIN_H_
-
+#ifndef _MAIN_H_
 #define _MAIN_H_
 
-#endif
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif //MAIN
