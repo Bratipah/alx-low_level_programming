8b7119932a0fa067716a9399abd12237df4e294d
Bratipah <bratipahmh@gmail.com>
2022-07-05 02:20:32 +0300

    Object Like Macro

---
+++ 0x0D-preprocessors/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif //MAIN
