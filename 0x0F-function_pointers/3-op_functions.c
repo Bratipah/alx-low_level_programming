d0e123e8be8387409610e7b2c6293753878282ae
Bratipah <bratipahmh@gmail.com>
2022-07-06 10:58:34 -0700

    goal to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -36,50 +36,24 @@ int op_mul(int a, int b)
 return (a * b);
 }
 
-
-
 /**
- *
- *  * op_div - calculates the division of two integers
- *
- *   * @a: first integer
- *
- *    * @b: second integer
- *
- *     *
- *
- *      * Return: result of the division of a and b
- *
- *       */
-
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the division of a and b
+ */
 int op_div(int a, int b)
-
 {
-
-		return (a / b);
-
+	return (a / b);
 }
 
-
-
 /**
- *
- *  * op_mod - calculates the remainder of the division of two integers
- *
- *   * @a: first integer
- *
- *    * @b: second integer
- *
- *     *
- *
- *      * Return: remainder of a divided b
- *
- *       */
-
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of a divided b
+ */
 int op_mod(int a, int b)
-
 {
-
-		return (a % b);
-
+	return (a % b);
 }
