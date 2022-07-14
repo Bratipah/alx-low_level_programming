b593bd0dc68c20333ee4b4921abea4be1d5eb46a
Bratipah <bratipahmh@gmail.com>
2022-07-14 11:42:20 -0700

    Node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,17 +1,11 @@
 #include "lists.h"
-
 #include <string.h>
-
-
 
 /**
  * _strlen - returns length of string
  * @s: string
  * Return: length of string
  */
-
-
-
 int _strlen(const char *s)
 {
 
@@ -33,27 +27,19 @@ int _strlen(const char *s)
 
 }
 
-
-
 /**
  * add_node - adds a new node at the beginning of  list_t list
  * @head: head of linked list
  * @str: string to be used as data for node
  * Return: number of elements
  */
-
-
-
 list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *new;
 
 	char *string;
-
 	int length;
-
-
 
 	new = malloc(sizeof(list_t));
 
@@ -62,35 +48,21 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	if (str == NULL)
-
 	{
-
 		string = NULL;
-
 		length = 0;
-
 	}
-
 	else
-
 	{
-
 		string = strdup(str);
 
 		length = _strlen(str);
-
 	}
 
 	new->len = length;
-
 	new->str = string;
-
 	new->next = *head;
-
 	*head = new;
 
-
-
 	return (*head);
-
 }
