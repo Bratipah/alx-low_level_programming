acbace0a7907c9ccf0db2a7610ef644a5f718e48
Bratipah <bratipahmh@gmail.com>
2022-07-14 11:25:05 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,19 +4,14 @@
 
 /**
  * print_list - Prints all the elements of a list
-
  * @h: Pointer to the list to be printed
-
  *
-
  * Return: The number of nodes printed
-
  */
 
 
 
 size_t print_list(const list_t *h)
-
 {
 
 	size_t nodes = 0;
