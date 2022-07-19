103644f8e6a0a9b986e6729665bd25b23031946c
Bratipah <bratipahmh@gmail.com>
2022-07-19 11:31:08 -0700

    A function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,6 @@
 /*
-
  * File: 0-print_listint.c
-
- * Auth: Gedeon Obae Gekonge
-
+ * Auth: Bratipah
  */
 
 
@@ -15,15 +12,9 @@
 
 
 /**
-
  * print_listint - Prints all the elements of a listint_t list.
-
  * @h: A pointer to the head of the list_t list.
-
- *
-
  * Return: The number of nodes in the list_t list.
-
  */
 
 size_t print_listint(const listint_t *h)
