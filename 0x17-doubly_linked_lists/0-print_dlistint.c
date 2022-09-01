5d2bbe444a8fe7a9ade7917228740be790c751b6
Bratipah <bratipahmh@gmail.com>
2022-09-01 14:21:15 -0700

    headers

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 
 
+
+
 /**
  * print_dlistint - prints a doubly linked list
  * @h: pointer to the list
@@ -13,9 +15,13 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t nodes = 0;
 
 
+
+
 	if (!h)
 
 		return (0);
+
+
 
 
 	while (h)
@@ -29,6 +35,9 @@ size_t print_dlistint(const dlistint_t *h)
 		nodes++;
 
 	}
+
+
+
 
 
 	return (nodes);
