b1c3615bd7cdc81f2d01af18fe644b2c26a6dece
Bratipah <bratipahmh@gmail.com>
2022-09-06 14:18:27 -0700

    add *c files and lib file

---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - calculate the length of a string
+ * @s: the string to measur
+ * Return: the string lengh
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+
+	++len;
+
+	return (len);
+
+}
