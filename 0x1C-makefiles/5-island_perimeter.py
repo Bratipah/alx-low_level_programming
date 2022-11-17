747adb515df39cf935d2b579291ee8206fe64a59
Bratipah <bratipahmh@gmail.com>
2022-11-17 04:25:13 -0800

    add make files

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that calculates the perimeter of an island in a grid."""
 
+
 def num_water_neighbors(grid, i, j):
     """Returns the number of water neighbors a cell has in a grid."""
 
